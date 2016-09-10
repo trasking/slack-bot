71925fe0838eb1015e8be7536353650f2777e0cf
jbt <jbt_5@yahoo.com>
2016-09-10 12:22:42 -0700

    basic working

--- handler.js
+++ handler.js
@@ -4,79 +4,36 @@ let slack = require('slack');
 let bot = require('bot');
 
 module.exports.command = (event, context, callback) => {
-	let botUrl = 'https://xb5qr6apmj.execute-api.us-west-2.amazonaws.com/dev/input';
-	event.body.callbackUrl = 'https://dpw5548x5e.execute-api.us-west-2.amazonaws.com/dev/callback';
+	
 	let request = {
-		text: event.body.text,
-		action: event.body.command,
-		payload: null,
-		context: event.body
-	}
-	bot.postToUrl(botUrl, request, (result) => {
-		callback(null, { function: 'command', result: result });	
+		input: {
+			text: event.body.text,
+			action: event.body.command,
+			payload: null
+		},
+		context: {
+			user: event.body.user_id,
+			callbackUrl: event.stageVariables.callbackUrl,
+			slackInfo: event.body
+		}
+	};
+
+	console.log(request);
+
+	bot.postToUrl(event.stageVariables.botUrl, request, (result) => {
+		callback(null, 'slack command finished');	
 	});
+
 }
 
 module.exports.callback = (event, context, callback) => {
-	let slackBody = slack.transformResponse(event.body);
-	bot.postToUrl(event.body.context.responseUrl, slackBody, (result) => {
+
+	console.log(event.body);
+	
+	let payload = slack.transformResponse(event.body);
+	
+	bot.postToUrl(event.body.context.slackInfo.response_url, payload, (result) => {
 		callback(null, { function: 'callback', result: result });	
 	});
+	
 }
-
-
-
-// {
-// 	"status": "cool",
-// 	"event": {
-// 		"body": {
-// 			"token": "cpz1VEunG2taKWJkOTjHccFV",
-// 			"team_id": "T1J329BG8",
-// 			"team_domain": "print-bot-test",
-// 			"channel_id": "C1J36UV0D",
-// 			"channel_name": "general",
-// 			"user_id": "U1J2YMMFY",
-// 			"user_name": "jbtrask",
-// 			"command": "/awesome",
-// 			"text": "",
-// 			"response_url": "https://hooks.slack.com/commands/T1J329BG8/78286688496/4uxMyTc9rqkMzR6MlppN0Fyj"
-// 		},
-// 		"method": "POST",
-// 		"principalId": "",
-// 		"stage": "dev",
-// 		"headers": {
-// 			"Accept": "application/json,*/*",
-// 			"Accept-Encoding": "gzip,deflate",
-// 			"CloudFront-Forwarded-Proto": "https",
-// 			"CloudFront-Is-Desktop-Viewer": "true",
-// 			"CloudFront-Is-Mobile-Viewer": "false",
-// 			"CloudFront-Is-SmartTV-Viewer": "false",
-// 			"CloudFront-Is-Tablet-Viewer": "false",
-// 			"CloudFront-Viewer-Country": "US",
-// 			"Content-Type": "application/x-www-form-urlencoded",
-// 			"Host": "dpw5548x5e.execute-api.us-west-2.amazonaws.com",
-// 			"User-Agent": "Slackbot 1.0 (+https://api.slack.com/robots)",
-// 			"Via": "1.1 70d79aa19e315b47281005f9e3c25c88.cloudfront.net (CloudFront)",
-// 			"X-Amz-Cf-Id": "xWk1beQSEYbUqTpCN_yxRIACa-a48r6ZB2keaLFuudtlhWqyK2TmXg==",
-// 			"X-Forwarded-For": "52.91.184.228, 54.239.145.61",
-// 			"X-Forwarded-Port": "443",
-// 			"X-Forwarded-Proto": "https"
-// 		},
-// 		"query": {},
-// 		"path": {},
-// 		"identity": {
-// 			"cognitoIdentityPoolId": "",
-// 			"accountId": "",
-// 			"cognitoIdentityId": "",
-// 			"caller": "",
-// 			"apiKey": "",
-// 			"sourceIp": "52.91.184.228",
-// 			"cognitoAuthenticationType": "",
-// 			"cognitoAuthenticationProvider": "",
-// 			"userArn": "",
-// 			"userAgent": "Slackbot 1.0 (+https://api.slack.com/robots)",
-// 			"user": ""
-// 		},
-// 		"stageVariables": {}
-// 	}
-// }
--- slack.js
+++ slack.js
@@ -1,6 +1,37 @@
 'use strict'
 
-module.exports.transformResponse = (frameworkResponse) => {
-	return { text: 'TODO: transform framework response to slack response' };	
+module.exports.transformResponse = (response) => {
+	let result = { text: response.text };
+	if (response.items) {
+		result.attachments = [];
+		for (var itemIdx = 0; itemIdx < response.items.length; itemIdx++) {
+			let info = response.items[itemIdx];
+			let item = { fallback: info.basic_text };
+			result.attachments.push(item);
+		}	
+
+	}
 };
 
+
+// {
+//      		callback_id: 'action_print', 
+//      		fields: [
+//         		{
+//         			title: 'Type',
+//         			value: options[:file]['pretty_type'],
+//         			short: true
+//         		},
+//         		{
+//         			title: 'Title',
+//         			value: options[:file]['title'],
+//         			short: true
+//         		},
+//         		{
+//         			title: 'Filename',
+//         			value: options[:file]['name'],
+//         			short: true
+//         		}
+//         	],
+//         	actions: actions
+//         }
