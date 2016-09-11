ee22442da18a59009f6ecb9d3bac87954cdf6a86
jbt <jbt_5@yahoo.com>
2016-09-10 17:34:51 -0700

    events and actions (wip)

--- handler.js
+++ handler.js
@@ -1,29 +1,78 @@
 'use strict';
 
-let slack = require('slack');
-let bot = require('bot');
+const querystring = require('querystring');
+const slack = require('slack');
+const bot = require('bot');
 
 module.exports.command = (event, context, callback) => {
 	
 	let request = {
 		input: {
 			text: event.body.text,
-			action: event.body.command,
+			action: event.body.command.slice(1),
 			payload: null
 		},
 		context: {
 			user: event.body.user_id,
-			callbackUrl: event.stageVariables.callbackUrl,
-			slackInfo: event.body
+			callback: event.stageVariables.callbackUrl,
+			slack: event.body
 		}
 	};
 
 	console.log(request);
 
 	bot.postToUrl(event.stageVariables.botUrl, request, (result) => {
-		callback(null, 'slack command finished');	
+			
 	});
 
+	callback(null, 'command done!');
+}
+
+module.exports.action = (event, context, callback) => {
+
+	let body = querystring.parse(event.body);
+	let payload = JSON.parse(body)
+	
+	let slackContext = {
+		team_id: payload.team.id,
+		team_domain: payload.team.domain,
+		channel_id: payload.channel.id,
+		channel_name: payload.channel.name,
+		user_id: payload.user.id,
+		user_name: payload.user.name,
+		response_url: payload.response_url 
+	}
+
+	let request = {
+		input: {
+			text: null,
+			action: payload.actions[0].name,
+			payload: JSON.parse(payload.actions[0].value)
+		},
+		context: {
+			user: payload.user.id,
+			callback: event.stageVariables.callbackUrl,
+			slack: slackContext
+		}
+	};
+
+	console.log(request);
+
+	bot.postToUrl(event.stageVariables.botUrl, request, (result) => {
+			
+	});
+
+	callback(null, 'dude');
+}
+
+module.exports.event = (event, context, callback) => {
+	console.log(event.body);
+	if (event.body.challenge) {
+		callback(null, { challenge: event.body.challenge } );
+	}
+	else {
+		callback();	
+	}
 }
 
 module.exports.callback = (event, context, callback) => {
@@ -32,8 +81,13 @@ module.exports.callback = (event, context, callback) => {
 	
 	let payload = slack.transformResponse(event.body);
 	
-	bot.postToUrl(event.body.context.slackInfo.response_url, payload, (result) => {
+	console.log(payload);
+	
+	bot.postToUrl(event.body.context.slack.response_url, payload, (result) => {
 		callback(null, { function: 'callback', result: result });	
 	});
 	
 }
+
+// sample action
+// payload=%7B%22actions%22%3A%5B%7B%22name%22%3A%22name%22%2C%22value%22%3A%22dude%22%7D%5D%2C%22callback_id%22%3A%22abc%22%2C%22team%22%3A%7B%22id%22%3A%22T1J329BG8%22%2C%22domain%22%3A%22print-bot-test%22%7D%2C%22channel%22%3A%7B%22id%22%3A%22C1J36UV0D%22%2C%22name%22%3A%22general%22%7D%2C%22user%22%3A%7B%22id%22%3A%22U1J2YMMFY%22%2C%22name%22%3A%22jbtrask%22%7D%2C%22action_ts%22%3A%221473549426.919446%22%2C%22message_ts%22%3A%221473549424.000017%22%2C%22attachment_id%22%3A%221%22%2C%22token%22%3A%22cpz1VEunG2taKWJkOTjHccFV%22%2C%22response_url%22%3A%22https%3A%5C%2F%5C%2Fhooks.slack.com%5C%2Factions%5C%2FT1J329BG8%5C%2F78382827094%5C%2FTcec2bqHoJFCekSbDCaYOqve%22%7D
--- slack.js
+++ slack.js
@@ -1,37 +1,56 @@
 'use strict'
 
 module.exports.transformResponse = (response) => {
-	let result = { text: response.text };
+	
+	let result = { 
+		text: response.text 
+	};
+
 	if (response.items) {
 		result.attachments = [];
 		for (var itemIdx = 0; itemIdx < response.items.length; itemIdx++) {
+			
 			let info = response.items[itemIdx];
-			let item = { fallback: info.basic_text };
+			
+			let item = { 
+				fallback: info.basic_text 
+			};
+	
+			if (info.color) item.color = info.color;
+			if (info.image)	item.thumb_url = info.image;
+			if (info.footer_text) item.footer = info.footer_text;
+
+			if (info.text_fields) {
+				item.fields = [];
+				for(var fieldIdx = 0; fieldIdx < info.text_fields.length; fieldIdx++) {
+					let field = { 
+						title: info.text_fields[fieldIdx].title,
+						value: info.text_fields[fieldIdx].text
+					}
+					item.fields.push(field);
+				}
+			}
+
+			if (info.actions) {
+				item.actions = [];
+				item.callback_id =  action_' + response.detail.action;
+				for(var actionIdx = 0; actionIdx < info.actions.length; actionIdx++) {
+					let actionInfo = info.actions[actionIdx];
+					let action = {
+						name: actionInfo.id,
+						text: actionInfo.text,
+						style: actionInfo.default ? 'primary' : 'default',
+						type: 'button',
+						value: JSON.stringify(actionInfo.payload)'
+					};
+					item.actions.push(action);
+					console.log(action);
+				}
+			}
+
 			result.attachments.push(item);
-		}	
-
+		}
 	}
+
+	return result;
 };
-
-
-// {
-//      		callback_id: 'action_print', 
-//      		fields: [
-//         		{
-//         			title: 'Type',
-//         			value: options[:file]['pretty_type'],
-//         			short: true
-//         		},
-//         		{
-//         			title: 'Title',
-//         			value: options[:file]['title'],
-//         			short: true
-//         		},
-//         		{
-//         			title: 'Filename',
-//         			value: options[:file]['name'],
-//         			short: true
-//         		}
-//         	],
-//         	actions: actions
-//         }
